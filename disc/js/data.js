1e3ae410fd8fed5ef963619966bbab3060a2b3b5
brunocriacoes <br.rafael@outlook.com>
2019-03-09 14:32:54 -0300

    ajuste nova plataforma

---
+++ disc/js/data.js
@@ -0,0 +1,18 @@
+var site = {
+    "title": "bruno criaçoes",
+    "shortTitle" : "BRC",
+    "slogan" : "transformando ideias em produtos"
+};
+
+var menu_site = [
+    { link: 'index.html#inicio', text: 'Home', ico: 'home'},
+    { link: 'index.html#sobre', text: 'Sobre', ico: ''},
+    { link: 'blog.html', text: 'Blog', ico: ''},
+    { link: 'index.html#cursos', text: 'Cursos', ico: ''},
+    { link: 'index.html#criacao-de-app', text: 'Criação de app', ico: ''},
+    { link: 'index.html#contato', text: 'contato', ico: ''}
+];
+
+var menu_curso = [
+    { link: '', text: '', ico: ''},
+];
